0e4c4e0b0e7a10202d0e16059c3511dca19e3027
priscilapotulnisky <potulniskyp@gmail.com>
2024-12-29 22:22:59 -0300

    Ejercicio 2

--- Prácticas/Práctica 1 - Parte 1/Ejercicio2.pas
+++ Prácticas/Práctica 1 - Parte 1/Ejercicio2.pas
@@ -4,7 +4,7 @@ var
 begin
 	write('Ingrese un numero: ');
 	readln(num);
-	if(num >0) then
+	if(num >= 0) then
 		writeln('El valor absoluto de ', num:2:0, ' es: ', num:2:0)
 	else
 		writeln('El valor absoluto de ', num:2:0, ' es: ', (num * -1):2:0);
