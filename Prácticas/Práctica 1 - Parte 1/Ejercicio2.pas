00b967819a8ea723559de13d3f3495c7b2eff9da
priscilapotulnisky <potulniskyp@gmail.com>
2024-12-29 22:20:50 -0300

    Ejercicio 2

---
+++ Prácticas/Práctica 1 - Parte 1/Ejercicio2.pas
@@ -0,0 +1,11 @@
+program ejercicio2;
+var
+	num: real;
+begin
+	write('Ingrese un numero: ');
+	readln(num);
+	if(num >0) then
+		writeln('El valor absoluto de ', num:2:0, ' es: ', num:2:0)
+	else
+		writeln('El valor absoluto de ', num:2:0, ' es: ', (num * -1):2:0);
+end.
