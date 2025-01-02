55a006e9ccf4a809ee6e018ee84173b89253ceae
priscilapotulnisky <potulniskyp@gmail.com>
2025-01-02 16:53:28 -0300

    Ejercicio 5

---
+++ Prácticas/Práctica 2 - Parte 1/Ejercicio5.pas
@@ -0,0 +1,13 @@
+program alcance4;
+function cuatro: integer;
+begin
+	cuatro:= 4;
+end;
+var 
+	a: integer;
+begin
+	cuatro;
+	writeln(a);
+end.
+
+//El error es que no está inicializada la variable "a". 
