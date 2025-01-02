a00a9383d85b8c2da86d240b79e1ca300bee3d59
priscilapotulnisky <potulniskyp@gmail.com>
2025-01-02 20:06:24 -0300

    Ejercicio 3

--- Prácticas/Práctica 2 - Parte 2/Ejercicio3.pas
+++ Prácticas/Práctica 2 - Parte 2/Ejercicio3.pas
@@ -19,11 +19,11 @@ begin
 	{ averigua si el resultado final estuvo entre 10 y 30}
 	ok := (result >= 10) or (result <= 30);
 	if (not ok) then
-		write ('La suma no quedó entre 10 y 30');
+		write ('La suma no quedó entre 10 y 30'); 
 end.
 //Error 1 --> El parametro c debe ser de referencia (linea 3)
 //Error 2 --> Se invoca mal al proceso (linea 17)
 //Error 3 --> No esta declarada la variable ok (linea 20)
 //Error 4 --> No se inicializa la variable suma (linea 8)
 //Error 5 --> En vez de "or" deberia ir "and" (linea 20)
-//Error 6 --> Falta validar que a <= b para que funcione 
+//Error 6 --> Falta validar que a <= b para que funcione (en el programa principal o en el proceso)
